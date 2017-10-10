51f5f3b64a4fbdc4f979806eccfbec65690d0843
Pete Russell <pinkbatz@gmail.com>
2017-10-10 14:53:58 +1300

    Adds basic example of observable triggered each second.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +13,21 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    /* There are different methods of choosing when Observables should be
+       emitted, in this case it emits an event at the interval (ms) passed
+       to interval(). */
+    const myNumbers = Observable.interval(1000);
+
+    /* Subscribe has three handlers, each being a function passed as an
+       argument to subscribe():
+        1. normal data handling
+        2. error handling
+        3. completed handling */
+    myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    )
   }
 
 }
